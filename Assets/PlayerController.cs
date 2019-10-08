626d95f477d861062200132919da0d31fb915ad8
Vinicius Aguiar <vinicius12ex@hotmail.com>
2019-10-08 12:00:59 -0300

    test of server connection
    testing the workflow of a server. When something happens in the local player, the server refresh the information for the other players

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,17 +5,67 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    public Material laranja;
+    public Material rosa;
+
+    public float velocidade = 300f;
+    public GameObject cuboPrefab;
+
     private void Start()
     {
+        Material material = null;
+
+        if(isServer && isLocalPlayer)
+        {
+            material = laranja;
+        }
+        else if(isServer && !isLocalPlayer)
+        {
+            material = rosa;
+        }
+        else if(!isServer && isLocalPlayer)
+        {
+            material = rosa;
+        }
+        else if(!isServer && !isLocalPlayer)
+        {
+            material = laranja;
+        }
+
+        var meshRender = GetComponent<MeshRenderer>();
+        meshRender.material = material;
+
+
     }
 
     private void Update()
     {
+
         if (isLocalPlayer)
         {
             var h = Input.GetAxis("Horizontal");
+            var v = Input.GetAxis("Vertical");
 
-            transform.Translate(Vector3.left * (h * Time.deltaTime));
+            transform.Translate(Vector3.forward * (v * Time.deltaTime));
+            transform.Rotate(Vector3.up * h * velocidade * Time.deltaTime);
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                CmdSpawnarCubo();
+            }
         }
+    }
+
+    [Command]
+    private void CmdSpawnarCubo()
+    {
+        var cubo = Instantiate(cuboPrefab);
+
+        cubo.transform.position = transform.position + (transform.forward * 2);
+        cubo.transform.rotation = transform.rotation;
+
+        NetworkServer.Spawn(cubo);
     }
 }
